f96206f4981129fd1330164d622e0f5a36111419
charlieroberts <charlie@charlie-roberts.com>
2016-10-12 00:46:37 -0400

    saving and loading of files

--- js/storage.js
+++ js/storage.js
@@ -6,7 +6,7 @@ var WorkshopStorage = {
   values: null,
   initialized: false,
 
-  init: function() {
+  init: function( peged, ed, pegText, edText ) {
     Storage.prototype.setObject = function( key, value ) { 
       this.setItem( key, JSON.stringify( value ) ); 
     }
@@ -23,13 +23,51 @@ var WorkshopStorage = {
         lastSavedState: {
           grammar:null,
           test:null
-        }
+        },
+        userFiles: {}
       }
 
-      //this.save()
     }
 
+    this.pegEditor  = peged
+    this.testEditor = ed
+    this.defaultPegText = pegText
+    this.defaultEditorText = edText
+
+    this.createGUI()
     this.initialized = true
+  },
+
+  createGUI: function() {
+    var saveBtn = document.querySelector( '#saveBtn' ),
+        loadMenu = document.querySelector( '#loadMenu' )
+
+    saveBtn.onclick = function() {
+      WorkshopStorage.getNameAndSave()
+    }
+
+    var userFileNames = Object.keys( this.values.userFiles )
+    for( var i = 0; i < userFileNames.length; i++ ){
+      let opt = document.createElement('option')
+      opt.innerText = userFileNames[ i ]
+      loadMenu.appendChild( opt )
+    }
+
+    loadMenu.onchange = function( e ) {
+      WorkshopStorage.loadFileWithName( e.target[ e.target.selectedIndex ].innerText ) 
+    }
+
+  },
+
+  getNameAndSave: function() {
+    var filename = window.prompt( 'Please enter a filename. Your file will be stored locally in your browser.' )
+
+    WorkshopStorage.values.userFiles[ filename ] = {
+      grammar: WorkshopStorage.pegEditor.getValue(),
+      test: WorkshopStorage.testEditor.getValue()
+    }
+
+    WorkshopStorage.save() 
   },
 
   save : function() {
@@ -42,6 +80,18 @@ var WorkshopStorage = {
 
   getFileWithName: function( name ) {
     return this.values.userFiles[ name ]
+  },
+
+  loadFileWithName: function( name ) {
+    if( name !== 'default' ) {
+      var file = WorkshopStorage.getFileWithName( name )
+
+      WorkshopStorage.pegEditor.setValue( file.grammar )
+      WorkshopStorage.testEditor.setValue( file.test )
+    }else{
+      WorkshopStorage.pegEditor.setValue( WorkshopStorage.defaultPegText )
+      WorkshopStorage.testEditor.setValue( WorkshopStorage.defaultEditorText )
+    }
   },
 
   saveFileWithName: function( name, txt ) {
